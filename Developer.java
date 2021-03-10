c9da36b45ae49a501d73fd775d3222bf709a2a9e
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-03-10 21:13:03 +0200

    Added Developer and ProjectManager

--- Developer.java
+++ Developer.java
@@ -1,3 +1,35 @@
+import java.util.HashSet;
+import java.util.stream.Stream;
+
 public class Developer {
-    HashSet<String> skills = new HashSet<>();
+    public HashSet<String> skills = new HashSet<>();
+    public String company;
+    public long bonus;
+
+    public Developer(HashSet<String> skills, String company, long bonus) {
+        this.skills = skills;
+        this.company = company;
+        this.bonus = bonus;
+    }
+
+    public long getWP(Developer d) {
+        long commonSkills = 0;
+        long reunionSkills = 0;
+        commonSkills = skills.stream().filter(skill -> {
+            return d.skills.contains(skill);
+        }).count();
+        reunionSkills = Stream.concat(skills.stream(), d.skills.stream()).count();
+        return commonSkills * (reunionSkills - commonSkills);
+    }
+
+    public long getBP(Developer d) {
+        if (company.equals(d.company)) {
+            return bonus * d.bonus;
+        }
+        return 0;
+    }
+
+    public long getTotal(Developer d) {
+        return getWP(d) + getBP(d);
+    }
 }
---
+++ ProjectManager.java
@@ -0,0 +1,9 @@
+import java.util.HashSet;
+
+public class ProjectManager extends Developer {
+
+    public ProjectManager(String company, long bonus) {
+        super(null, company, bonus);
+    }
+
+}
